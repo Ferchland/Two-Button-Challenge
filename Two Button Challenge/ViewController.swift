cfce9cab4432effe4dc69a621277239e46a636b6
Andrew Ferchland <aferchland@me.com>
2023-08-03 10:52:31 -0400

    Ch 2.3 Two Button Challenge

--- Two Button Challenge/ViewController.swift
+++ Two Button Challenge/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var defaultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        defaultLabel.text = "What are you?"
     }
 
-
+    @IBAction func showMessageButtonPressed(_ sender: UIButton) {
+        defaultLabel.text = "You are awesome!"
+    }
+    @IBAction func showAnotherMessageButtonPressed(_ sender: UIButton) {
+        defaultLabel.text = "You are amazing!"
+    }
+    
 }
 
